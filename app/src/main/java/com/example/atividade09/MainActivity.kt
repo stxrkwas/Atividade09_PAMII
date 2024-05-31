710756b3bafc2afd7fea5354a136e9e3cad9509e
stxrkwas <maria.silva4476@etec.sp.gov.br>
2024-05-30 21:40:41 -0300

    Final version - 1.0

--- app/src/main/java/com/example/atividade09/MainActivity.kt
+++ app/src/main/java/com/example/atividade09/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.atividade09
 // Classe para gerenciar o estado da activity
 import android.os.Bundle
 
+// Classe para exibir mensagens no Logcat
+import android.util.Log
+
+import android.nfc.Tag
+
 // Função para habilitar o modo edge-to-edge
 import androidx.activity.enableEdgeToEdge
 
@@ -17,6 +22,9 @@ import androidx.compose.material3.Surface
 
 // Componente para exibir texto
 import androidx.compose.material3.Text
+
+// Componente para exibir um campo de texto
+import androidx.compose.material3.TextField
 
 // Classe base para activities no Jetpack Compose
 import androidx.activity.ComponentActivity
@@ -33,8 +41,23 @@ import androidx.compose.foundation.background
 // Modificadores para layout de elementos
 import androidx.compose.foundation.layout.*
 
+// Componente para exibir botões
+import androidx.compose.material3.Button
+
 // Anotation para funções que compõem a UI
 import androidx.compose.runtime.Composable
+
+//
+import androidx.compose.runtime.getValue
+
+//
+import androidx.compose.runtime.mutableStateOf
+
+//
+import androidx.compose.runtime.remember
+
+//
+import androidx.compose.runtime.setValue
 
 // Classe para alinhamento de elementos
 import androidx.compose.ui.Alignment
@@ -57,11 +80,20 @@ import androidx.compose.ui.unit.dp
 // Classe para definir unidades de medida para tamanho de texto
 import androidx.compose.ui.unit.sp
 
-// Classe para gerenciar elementos de navegação
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import com.example.atividade09.ui.theme.DebugButtonColors
+import com.example.atividade09.ui.theme.ErrorButtonColors
+import com.example.atividade09.ui.theme.InfoButtonColors
+import com.example.atividade09.ui.theme.WarningButtonColors
 
 // Classe para definir o tema da aplicação
 import com.example.atividade09.ui.theme.Atividade09Theme
+
+// Criando a variável com o valor do TAG
+const val TAG = "TestAndroid"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +101,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Atividade09Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     NewApp()
                 }
             }
@@ -79,13 +114,166 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewApp() {
+
+    var username by remember{
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
-            .background(Color())
+            .padding(top = 200.dp)
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ){
 
+        Text(
+            text = "Atividade de PAM II",
+            color = Color.Black,
+            fontSize = 30.sp
+        )
+
+        // Adiciona um espaço vertical.
+        Spacer(modifier = Modifier.height(10.dp))
+
+        val image = painterResource(R.drawable.logo_etec)
+        
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .height(120.dp)
+                .width(150.dp)
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        TextField(
+            value = username,
+            onValueChange = { novoValor -> username = novoValor},
+            label = { Text("Digite seu nome")}
+        )
+
+        // Adiciona um espaço vertical.
+        Spacer(modifier = Modifier.height(25.dp))
+
+        // Cria os botões
+        ActionButton(
+
+            // Define o texto do botão
+            text = "Nota 1",
+
+            // Chaam a função que define as cores dos botões
+            buttonColors = DebugButtonColors(),
+
+            // Define o tamanho do botão
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ){
+            // Imprime no Logcat o texto do botão
+            Log.d(TAG, "$username: Tirou MB!")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Adiciona mais um botão
+        ActionButton(
+
+            // Define o texto do botão
+            text = "Nota 2",
+
+            // Chama a função que define as cores dos botões
+            buttonColors = InfoButtonColors(),
+
+            // Define o tamanho do botão
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ){
+
+            // Imprime no Logcat o texto do botão
+            Log.i(TAG, "$username: Tirou B!")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Adiciona mais um botão
+        ActionButton(
+
+            // Define o texto do botão
+            text = "Nota 3",
+
+            // Chama a função que define as cores dos botões
+            buttonColors = WarningButtonColors(),
+
+            // Define o tamanho do botão
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ){
+
+            // Imprime no Logcat o texto do botão
+            Log.w(TAG, "$username: Tirou R!")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Adiciona mais um botão
+        ActionButton(
+
+            // Define o texto do botão
+            text = "Nota 4",
+
+            // Chama a função que define as cores dos botões
+            buttonColors = ErrorButtonColors(),
+
+            // Define o tamanho do botão
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ){
+
+            // Lê o conteúdo do botão e lança uma exceção
+            try{
+
+                // Cria uma exceção
+                throw RuntimeException("Tirou I!")
+
+            }
+
+            // Captura a exceção
+            catch(ex: Exception){
+
+                // Imprime no Logcat a exceção
+                Log.e(TAG, "$username: ${ex.message}")
+            }
+        }
+    }
+}
+
+@Composable
+fun ActionButton(
+
+    // Declara as variáveis
+    text: String,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
+    modifier: Modifier = Modifier,
+    block: () -> Unit
+){
+
+    // Define o tipo do botão
+    ElevatedButton(
+
+        // Define a ação do botão
+        onClick = block,
+
+        // Define a forma do botão
+        shape = RoundedCornerShape(10.dp),
+
+        // Define as cores do botão
+        colors = buttonColors,
+
+        // Define o tamanho do botão
+        modifier = modifier
+    ) {
+
+        // Exibe o texto do botão
+        Text(text = text)
     }
 }
 
@@ -93,6 +281,6 @@ fun NewApp() {
 @Composable
 fun GreetingPreview() {
     Atividade09Theme {
-        Greeting("Android")
+        NewApp()
     }
 }
--- app/src/main/java/com/example/atividade09/ui/theme/Theme.kt
+++ app/src/main/java/com/example/atividade09/ui/theme/Theme.kt
@@ -3,13 +3,18 @@ package com.example.atividade09.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -50,9 +55,45 @@ fun Atividade09Theme(
         else -> LightColorScheme
     }
 
+    // Define a view
+    val view = LocalView.current
+
+    // Verifica se a view é em modo de edição
+    if(!view.isInEditMode){
+
+        // Define o efeito colateral
+        SideEffect{
+
+            // Obtém a janela da atividade atual
+            val window = (view.context as Activity).window
+
+            // Define a cor do da barra de status para a cor priméria do esquema de cores
+            window.statusBarColor = colorScheme.primary.toArgb()
+
+            // Define se a barra de status deve ter uma aparência clara ou escura.
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
 }
+
+// Cria uma funçao para o botão de debug
+@Composable
+fun DebugButtonColors() = ButtonDefaults.buttonColors(containerColor = DarkGreen)
+
+// Cria uma funçao para o botão de aviso
+@Composable
+fun WarningButtonColors() = ButtonDefaults.buttonColors(containerColor = DarkOrange)
+
+// Cria uma funçao para o botão de erro
+@Composable
+fun ErrorButtonColors() = ButtonDefaults.buttonColors(containerColor = DarkRed)
+
+// Cria uma funçao para o botão de informação
+@Composable
+fun InfoButtonColors() = ButtonDefaults.buttonColors(containerColor = DarkBlue)
